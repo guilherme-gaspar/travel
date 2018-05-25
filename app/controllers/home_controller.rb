8676930e8247ef5a27228aa00b3fc595e0bac429
Guilherme Gaspar <guilherme.c.gaspargg@gmail.com>
2018-05-25 12:24:59 -0300

    Layout for home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
   def index
     # Numero + Logradouro + City + State + Country
     # @x = Geocoder.coordinates("1896 Rua Sepé, Capão da Canoa, RS, BR")
