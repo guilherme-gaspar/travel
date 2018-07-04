f881aedd0a45b28ebfa54369e27c2e6487575902
Guilherme Gaspar <guilherme.c.gaspargg@gmail.com>
2018-07-04 01:27:21 -0300

    Writing documentation

--- spec/controllers/documentations_controller_spec.rb
+++ spec/controllers/documentations_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe DocumentationsController, type: :controller do
-
+  it "Responde corretamente" do
+    get :index
+    expect(response).to be_success
+  end
+  it "Responde corretamente" do
+    get :doc_pagseguro
+    expect(response).to be_success
+  end
+  it "Responde corretamente" do
+    get :doc_how_to_use
+    expect(response).to be_success
+  end
+  it "Responde corretamente" do
+    get :doc_tips
+    expect(response).to be_success
+  end
+  it "Retorna uma resposta 200" do
+    get :index
+    expect(response). to have_http_status "200"
+  end
 end
