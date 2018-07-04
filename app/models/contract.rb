d1200315ead87834717271dafc1722082128cb6a
Guilherme Gaspar <guilherme.c.gaspargg@gmail.com>
2018-07-04 00:18:11 -0300

    Production adjustment

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,6 +2,7 @@ class Contract < ActiveRecord::Base
 
   validates :number_installments, presence: true
   validates :value, presence: true
+  validates :due_date, presence: true
 
   belongs_to :user
   belongs_to :admin
